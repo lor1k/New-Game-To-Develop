ab7f967ac3302ea626dd7db7fcad93d31a4bc1a8
Alex <shhhook04@gmail.com>
2018-09-22 15:12:00 +0300

    Create
    Create

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MainClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Best game";
+		config.width = 1024;
+		config.height = 720;
+		new LwjglApplication(new MainClass(), config);
+	}
+}
