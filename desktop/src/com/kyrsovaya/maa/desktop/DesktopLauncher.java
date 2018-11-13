d60983002ec97a3553f9ead6e42a39181b21c7b8
Alex <shhhook04@gmail.com>
2018-11-13 19:30:52 +0200

    New Project
    add

---
+++ desktop/src/com/kyrsovaya/maa/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kyrsovaya.maa.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kyrsovaya.maa.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
