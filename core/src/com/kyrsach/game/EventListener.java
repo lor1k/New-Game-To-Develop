ff40f6c8b9b9478658665eef97b4b9dc213540b3
Yevgenii <shlapak.oleh@gmail.com>
2018-11-16 12:50:29 +0200

    Animation commit
    Cool animation 4u!

---
+++ core/src/com/kyrsach/game/EventListener.java
@@ -0,0 +1,47 @@
+package com.kyrsach.game;
+
+import com.badlogic.gdx.InputProcessor;
+
+public class EventListener implements InputProcessor {
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		Main.Warior obj = new Main.Warior(screenX, screenX);
+		return true;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
+	}
+}
--- core/src/com/kyrsach/game/Main.java
+++ core/src/com/kyrsach/game/Main.java
@@ -1,33 +1,55 @@
 package com.kyrsach.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+
+	public static class Warior{
+			public int x;
+			public int y;
+		public Warior(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+		public void Spawn(int x, int y){
+			Animator anim = new Animator(x, y);
+			anim.create();
+		}
+	}
+
+
+	Animator anim = new Animator(200, 200);
+	EventListener listener = new EventListener();
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		anim.create();
 	}
+
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.input.setInputProcessor(listener);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		anim.render();
+
 	}
-	
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+		anim.dispose();
 	}
 }
