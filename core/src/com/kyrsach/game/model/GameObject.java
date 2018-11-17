00058f6946e1a297401eaa2d005173c768b1f044
Alex <shhhook04@gmail.com>
2018-11-17 21:29:52 +0200

    Создан GameObject

---
+++ core/src/com/kyrsach/game/model/GameObject.java
@@ -0,0 +1,41 @@
+package com.kyrsach.game.model;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class GameObject {
+
+    public float x;
+    public float y;
+    public float width;
+    public float height;
+
+    Sprite object;
+
+    public GameObject(float x, float y, float width, float height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    public float getX(){return x;}
+    public float getY(){return y;}
+    public float getWidth(){return width;}
+    public float getHeight(){return height;}
+
+    public void setX(float x){this.x = x;}
+    public void setY(float y){this.y = y;}
+    public void setWidth(float width){this.width = width;}
+    public void setHeight(float height){this.height = height;}
+
+    public void move(float x,float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(SpriteBatch batch){
+        object.setBounds(x,y,width,height);
+        object.draw(batch);
+    }
+
+}
--- core/src/com/kyrsach/game/view/GameScreen.java
+++ core/src/com/kyrsach/game/view/GameScreen.java
@@ -2,24 +2,29 @@ package com.kyrsach.game.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen implements Screen {
 
     private Texture texture;
+    private Texture worker;
     private SpriteBatch batch;
 
     @Override
     public void show() {
         batch = new SpriteBatch();
         texture = new Texture(Gdx.files.internal("bg.png"));
+        worker = new Texture(Gdx.files.internal("worker.png"));
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(texture, 0, 0);
+        batch.draw(worker, 0, ((Gdx.graphics.getHeight()/100f)*30f));
         batch.end();
     }
 
@@ -46,6 +51,7 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         texture.dispose();
+        worker.dispose();
         batch.dispose();
     }
 }
