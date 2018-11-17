27988d2256555fe2ff5ff500d2e193a933c39cbe
Alex <shhhook04@gmail.com>
2018-11-17 19:06:08 +0200

    Изменение архитектуры(NVC)
    Паттерн програмирования НВС

--- core/src/com/kyrsach/game/Main.java
+++ core/src/com/kyrsach/game/Main.java
@@ -1,55 +1,17 @@
 package com.kyrsach.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.kyrsach.game.view.GameScreen;
 
 
-public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Main extends Game {
 
-
-	public static class Warior{
-			public int x;
-			public int y;
-		public Warior(int x, int y){
-			this.x = x;
-			this.y = y;
-		}
-		public void Spawn(int x, int y){
-			Animator anim = new Animator(x, y);
-			anim.create();
-		}
-	}
-
-
-	Animator anim = new Animator(200, 200);
-	EventListener listener = new EventListener();
-	@Override
-	public void create () {
-		anim.create();
-	}
-
+	private Screen gameScreen;
 
 	@Override
-	public void render () {
-		Gdx.input.setInputProcessor(listener);
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		anim.render();
-
-	}
-
-	@Override
-	public void dispose () {
-
-		anim.dispose();
+	public void create() {
+		gameScreen = new GameScreen();
+		setScreen(gameScreen);
 	}
 }
