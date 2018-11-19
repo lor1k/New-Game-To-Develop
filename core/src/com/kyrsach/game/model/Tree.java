a632aba1f107eff1549f8e42e814d0452228ad92
Yevgenii <shlapak.oleh@gmail.com>
2018-11-19 13:30:11 +0200

    Rendering trees
    Jeka - otec

---
+++ core/src/com/kyrsach/game/model/Tree.java
@@ -0,0 +1,10 @@
+package com.kyrsach.game.model;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Tree extends GameObject{
+
+    public Tree(float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+}
--- core/src/com/kyrsach/game/view/GameScreen.java
+++ core/src/com/kyrsach/game/view/GameScreen.java
@@ -6,6 +6,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.kyrsach.game.model.GameObject;
+import com.kyrsach.game.model.Tree;
+
+import java.util.Random;
 
 public class GameScreen implements Screen {
 
@@ -13,18 +17,36 @@ public class GameScreen implements Screen {
     private Texture sidebarleft;
     private Texture sidebarright;
     private Texture worker;
+    private Texture tree;
     private SpriteBatch batch;
-
+    private GameObject[] trees;
+    private final int count = 100;
+    private float left_spawn_border;
+    private float right_spawn_border;
+    private float top_spawn_border;
+    private float bottom_spawn_border;
+    private int tree_height = 70;
+    private int tree_width = 35;
+    private int a = 0;
     private BitmapFont FontRed1;
     private String str;
 
     @Override
     public void show() {
+        left_spawn_border = Gdx.graphics.getWidth()/100f*30f;
+        right_spawn_border = Gdx.graphics.getWidth() - left_spawn_border*2;
+        top_spawn_border = Gdx.graphics.getHeight();
+        bottom_spawn_border = 0;
         batch = new SpriteBatch();
         texture = new Texture(Gdx.files.internal("bg.png"));
         sidebarleft = new Texture(Gdx.files.internal("side_bar_left.png"));
         sidebarright = new Texture(Gdx.files.internal("side_bar_right.png"));
         worker = new Texture(Gdx.files.internal("worker.png"));
+        tree = new Texture(Gdx.files.internal("tree.png"));
+        trees = new Tree[count];
+        for (int i = 0; i < count; i++){
+            trees[i] = new Tree(left_spawn_border+(float)Math.random()*(right_spawn_border - tree_width),bottom_spawn_border+(float)Math.random()*(top_spawn_border - tree_height),tree.getWidth(), worker.getHeight());
+        }
 
         FontRed1 = new BitmapFont();
         FontRed1.setColor(1,0,0,1);
@@ -36,10 +58,19 @@ public class GameScreen implements Screen {
         batch.begin();
         batch.draw(texture, 0, 0);
         batch.draw(sidebarleft, 0,0);
-        str = ""+Gdx.graphics.getWidth() + "TEXT: " + sidebarright.getWidth() + "FOR" + Gdx.graphics.getWidth()/100f*83f;
+        str = "FPS: " + 1/delta;
         batch.draw(sidebarright,Gdx.graphics.getWidth() - sidebarright.getWidth(),0);
         batch.draw(worker, Gdx.graphics.getWidth()/100f*1f, Gdx.graphics.getHeight()/100f*30f);
         FontRed1.draw(batch, str, 10, 20);
+        for (int i = 0; i < a; i++){// Тут до count в оригинале
+            batch.draw(tree, trees[i].getX(), trees[i].getY(), tree_width,tree_height);
+        }
+
+        if(a == count-1){
+            a = count - 1;
+        } else{
+            a++;
+        }
         batch.end();
     }
 
@@ -71,6 +102,7 @@ public class GameScreen implements Screen {
         sidebarright.dispose();
         FontRed1.dispose();
         texture.dispose();
+        tree.dispose();
 
     }
 }
